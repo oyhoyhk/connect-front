c407fe41aa5ed052be1df47853839e8bbf48e618
oyhoyhk <oyhoyhk@naver.com>
2022-05-05 09:28:09 +0900

    1차 배포

--- src/lib/sockets/chatHallSocket.js
+++ src/lib/sockets/chatHallSocket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 let uid = localStorage.user ? JSON.parse(localStorage.user).uid : null;
-export const socket = io('http://localhost:4000', {
+export const socket = io('http://13.125.200.108:4000', {
   query: { uid },
   closeOnBeforeunload: false,
   cors: { origin: '*' },
